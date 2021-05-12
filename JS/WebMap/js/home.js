fa952535df39f4ed6304991468a112e0fe1e645c
Xinyi Qiu <66391148+Xy16-5@users.noreply.github.com>
2021-05-11 22:43:12 -0400

    js update

--- JS/WebMap/js/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#button1').click(function(e){
-        window.location.href="./main.html";
-    })
-})
-
-$(document).ready(function(){
-    $('.toast').toast('show');
-  });  
